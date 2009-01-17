1860cff1cdc196210396c9641aa1afbb8a41789d
Matt Swasey <mig@pggbee.com>
2009-01-17 01:11:36 -0500

    added magit and textile modes

--- emacs-modes.el
+++ emacs-modes.el
@@ -1,1 +1,11 @@
 ;; emacs-modes.el - loading different modes to extend emacs
+
+(load-lib-dir "modes")
+
+; Magit
+(require 'magit)
+
+; Textile
+(require 'textile-mode)
+(add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
+
--- init.el
+++ init.el
@@ -3,7 +3,12 @@
 (require 'cl)
 
 (defvar lib-dir "~/.emacs.d/")
-(add-to-list 'load-path lib-dir)
+(defun load-lib (name)
+  (load (concat lib-dir name ".el")))
+(defun load-lib-dir (path)
+  (add-to-list 'load-path (concat lib-dir path)))
+
+(load-lib-dir lib-dir)
 
 (setq inhibit-startup-message t)
 (setq make-backup-files nil)
@@ -13,10 +18,6 @@
 (setq mac-emulate-three-button-mouse nil)
 (prefer-coding-system 'utf-8)
 
-(defun load-lib (name)
-  (load (concat lib-dir name ".el")))
-
 (when window-system (load-lib "emacs-gui"))
-(load-lib "emacs-key-bindings" "emacs-modes")
-;(load (concat lib-dir "emacs-key-bindings.el"))
-;(load (concat lib-dir "emacs-modes.el"))
+(load-lib "emacs-key-bindings")
+(load-lib "emacs-modes")
