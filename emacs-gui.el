e29daf7bb1994383ecd6e95c80b2b47f375dc34f
Matt Swasey <mig@pggbee.com>
2009-01-18 13:36:02 -0500

    color-theme-merbivore is now a submodule

--- emacs-gui.el
+++ emacs-gui.el
@@ -20,7 +20,9 @@
 
 ;; color theme - with my merbivore theme applied
 (load-lib-dir "vendor/color-theme")
+(load-lib-dir "vendor/color-theme-merbivore")
 (require 'color-theme)
 (color-theme-initialize)
 (setq color-theme-is-global t)
+(load-file (concat root-dir "/vendor/color-theme-merbivore/color-theme-merbivore.el"))
 (color-theme-merbivore)
