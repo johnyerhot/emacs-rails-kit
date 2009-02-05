7ce871d0c1746cdd02bc45ba0e347904bf9384bf
Matt Swasey <mig@pggbee.com>
2009-02-05 16:25:06 -0500

    added dark and light modes, added refresh buffer

--- emacs-gui.el
+++ emacs-gui.el
@@ -13,16 +13,36 @@
 
 (column-number-mode t)
 
-;; set the size of the emacs frame
-(setq default-frame-alist 
-      '((width . 120) 
-        (height . 40)))
+;; Full screen toggle 
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+(global-set-key (kbd "M-n") 'toggle-fullscreen)
 
-;; color theme - with my merbivore theme applied
+;; set emacs to dark mode
+(defun dark-theme ()
+  (set-background-color "black")
+  (set-foreground-color "white"))
+
+(defun light-theme ()
+  (set-background-color "white")
+  (set-foreground-color "black"))
+
+(defun refresh-file ()
+  (interactive)
+  (revert-buffer t t t))
+
+(global-set-key [f5] 'refresh-file)
+
+;; color theme - with my merbivore theme included
 (load-lib-dir "vendor/color-theme")
 (load-lib-dir "vendor/color-theme-merbivore")
 (require 'color-theme)
 (color-theme-initialize)
 (setq color-theme-is-global t)
 (load-file (concat root-dir "/vendor/color-theme-merbivore/color-theme-merbivore.el"))
-(color-theme-merbivore)
+
+;; load a theme like this
+; (color-theme-merbivore)
