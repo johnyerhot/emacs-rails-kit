27169b2e719231d56d0fa14ca2be06b00336a986
Matt Swasey <mig@pggbee.com>
2009-02-09 15:50:55 -0500

    removed font setting from my user config, and made it machine config (not in git, sorry it's private)

--- mig.el
+++ mig.el
@@ -1,8 +1,5 @@
 ;; mig.el - personal stuff
 
-;(set-face-font 'default "-apple-inconsolata-medium-r-normal--16-0-72-72-m-0-iso10646-1")
-(set-face-font 'default "-apple-dejavu sans mono-medium-r-normal--13-0-72-72-m-0-iso10646-1")
-
 ; Map command-x,c,v to cut, copy, paste
 (global-set-key (kbd "C-w") 'clipboard-kill-region)
 (global-set-key (kbd "M-w") 'clipboard-kill-ring-save)
