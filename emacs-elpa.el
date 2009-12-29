4ec6e1c2ddf9d53c5f129e3153e2e0e0d4ea3bd1
Matt Swasey <matt.swasey@gmail.com>
2009-12-28 23:26:00 -0500

    elpa changes

--- emacs-elpa.el
+++ emacs-elpa.el
@@ -1,17 +1,32 @@
 ;; emacs-elpa.el - loading and configuring the emacs list package archive
 
-;; Here is the package kit, what you install with it is up to you, there
-;; will probably be some overlap though in what I provide in the modes
-;; directory and what the elpa offers, not sure what to do about that
-;; i will not including anything in the elpa directory, it's simply there
-;; for you to install to.
-;;
-;; I'm not totally comfortable with how i am doing this, but it seems the
-;; best solution till another presents itself.
-;;
-;; Why?  Well, the ELPA is very convenient
 (load-lib "package")
 (load-lib-dir "elpa")
 (require 'package)
 (setq package-user-dir (concat root-dir "elpa"))
 (package-initialize)
+
+;; these packages will install when you first start emacs-rails-kit
+(defvar auto-install-packages (list 
+                               'css-mode
+                               'full-ack
+                               'ruby-mode 
+                               'rinari 
+                               'rspec-mode
+                               'textmate 
+                               'yaml-mode
+                               'yasnippet)
+  "Libraries that should be installed by default.")
+
+(defun starter-kit-elpa-install ()
+  "Install all starter-kit packages that aren't installed."
+  (interactive)
+  (dolist (package starter-kit-packages)
+    (unless (functionp package)
+      (package-install package))))
+
+;; On your first run, this should pull in all the base packages.
+;; But you might not be online, so ignore errors.
+(ignore-errors
+  (message "Checking base list of packages...")
+  (starter-kit-elpa-install))
