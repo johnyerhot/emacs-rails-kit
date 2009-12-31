374d84e0720dfae6c02cefef2c666cd784790775
Matt Swasey <matt.swasey@gmail.com>
2009-12-31 09:21:50 -0500

    Added markdown mode, got rid of some other stuff that's now in elpa

--- emacs-vendor.el
+++ emacs-vendor.el
@@ -1,5 +1,4 @@
 ;; emacs-vedor.el - general purpose modes to extend emacs
-
 (load-lib-dir "vendor")
 
 ;; Interactively Do Things (ido) emacs is more fun with ido fo real
@@ -10,26 +9,7 @@
 ;; Unbound - describe-unbound-keys
 (require 'unbound)
 
-;; Mode Compile
-(autoload 'mode-compile "mode-compile"
-  "Command to compile current buffer file based on the major mode" t)
-(global-set-key "\C-cc" 'mode-compile)
-(autoload 'mode-compile-kill "mode-compile"
-  "Command to kill a compilation launched by `mode-compile'" t)
-(global-set-key "\C-ck" 'mode-compile-kill)
-
-(require 'el-expectations)
-
-;; ack (make sure ack is in emacs path)
-(autoload 'ack-same "full-ack" nil t)
-(autoload 'ack "full-ack" nil t)
-(autoload 'ack-find-same-file "full-ack" nil t)
-(autoload 'ack-find-file "full-ack" nil t)
-
 (global-set-key (kbd "M-F") 'ack)
-
-;; Magit
-(require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (eval-after-load 'magit
@@ -41,8 +21,11 @@
 (require 'textile-mode)
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
 
-;; CSS
-(require 'css-mode)
+;; Markdown
+(autoload 'markdown-mode "markdown-mode/markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+   (cons '("\\.text" . markdown-mode) auto-mode-alist))
 
 ;; Javascript (espresso)
 (autoload 'espresso-mode "espresso" nil t)
@@ -52,9 +35,3 @@
 
 ;; Cheat - for using cheat.errtheblog.com for some reference
 (require 'cheat)
-
-;; Gist - learn to use this within emacs and it WILL help you
-;; it will use your ~/.gitconfig if it finds one
-;; see: http://github.com/blog/180-local-github-config
-(require 'gist)
-
